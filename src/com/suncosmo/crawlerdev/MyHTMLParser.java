575d76b413ecc605fb067900f8b34f057152f676
lalamida <suncosmo@163.com>
2017-04-19 00:03:58 +0800

    update http parser

--- src/com/suncosmo/crawlerdev/MyHTMLParser.java
+++ src/com/suncosmo/crawlerdev/MyHTMLParser.java
@@ -15,20 +15,41 @@ public class MyHTMLParser implements Runnable{
 
     public MyHTMLParser() {
 
-        this.srcKeyWords.add("zhaofuli");
+    }
 
-        this.srcNoWords.add("youfanhao");
+    private static Set<String> srcKeyWords;
 
-        this.srcNoWords.add("xiachedan");
+    private static Set<String> srcNoWords;
 
-        this.srcNoWords.add("php");
+    private static String srcAtt = "href", imgTag = "<img", imgAtt = "src";
+
+    private static String method = "GET";
+
+    private static Map<String, String> header;
+
+    static {
+
+        srcKeyWords = new HashSet<>();
+
+        srcKeyWords.add("zhaofuli");
+
+        srcNoWords = new HashSet<>();
+
+        srcNoWords.add("youfanhao");
+
+        srcNoWords.add("xiachedan");
+
+        srcNoWords.add("php");
+
+        header = new HashMap<>();
+
+        header.put("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6");
+
+        header.put("Referer", "http://zhaofuli.biz/luyilu/2016/1023/1112.html");
+
     }
 
     public void getAndParseHTML(String srcUrl) {
-        String method = "GET";
-        Map<String, String> header = new HashMap<>();
-        header.put("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6");
-        header.put("Referer", "http://zhaofuli.biz/luyilu/2016/1023/1112.html");
         HttpEvent.HttpConn httpConn = new HttpEvent(). new HttpConn(method, srcUrl, header, "");
         httpConn.setUrl(srcUrl);
         try {
@@ -36,82 +57,96 @@ public class MyHTMLParser implements Runnable{
             if(is != null) {
                 BufferedReader bufin = new BufferedReader(
                         new InputStreamReader(is));
-                String srcAtt = "href", imgTag = "<img", imgAtt = "src";
                 for (String tempLine; (tempLine = bufin.readLine()) != null; ) {
-                    int li;
+                    int srcI = 0, imgI = 0;
                     do {
-                        if ((li = tempLine.indexOf(srcAtt)) > 0) {
-                            tempLine = tempLine.substring(li);
-                            li = 6;
-                            while (li < tempLine.length() && tempLine.charAt(li) != '\"'
-                                    && tempLine.charAt(li) != '\'') {
-                                ++li;
-                            }
-                            String newSrcUrl = tempLine.substring(6, li);
-                            tempLine = tempLine.substring(li);
-                            if (newSrcUrl.charAt(0) == '/') {
-                                newSrcUrl = MAIN_DOMAIN + newSrcUrl;
-                            } else if (newSrcUrl.length() < 13 && newSrcUrl.indexOf("htm") > 0) {
-                                newSrcUrl = srcUrl.substring(0, srcUrl.lastIndexOf("/") + 1) + newSrcUrl;
-                            }
-                            boolean isNeeded = true;
-                            for (String s : srcKeyWords) {
-                                if (newSrcUrl.indexOf(s) > 0) {
-                                    isNeeded = true;
-                                    break;
-                                }
-                            }
-                            for (String s : srcNoWords) {
-                                if (newSrcUrl.indexOf(s) > 0) {
-                                    isNeeded = false;
-                                    break;
-                                }
-                            }
-                            if (oldSrcUrls.contains(newSrcUrl)
-                                    || newSrcUrl.length() == 0
-                                    || !newSrcUrl.contains("http")
-                                    || !newSrcUrl.contains("zhaofuli")) {
-                                isNeeded = false;
-                            }
-                            if (isNeeded) {
-                                System.out.println("src url: " + newSrcUrl);
-                                srcUrls.push(newSrcUrl);
-                                oldSrcUrls.add(newSrcUrl);
-                            }
+                        srcI = tempLine.indexOf(srcAtt);
+                        imgI = tempLine.indexOf(imgTag);
+                        if(srcI > 0 && imgI > 0) {
+                            tempLine = (srcI < imgI) ? parseImg(parseSrc(tempLine, srcUrl)) : parseSrc(parseImg(tempLine), srcUrl);
+                        } else if(srcI > 0) {
+                            tempLine = parseSrc(tempLine, srcUrl);
+                        } else if(imgI > 0) {
+                            tempLine = parseImg(tempLine);
                         }
-                        if ((li = tempLine.indexOf(imgTag)) > 0) {
-                            tempLine = tempLine.substring(li);
-                            tempLine = tempLine.substring(tempLine.indexOf(imgAtt));
-                            li = 5;
-                            while (li < tempLine.length() && tempLine.charAt(li) != '\"' && tempLine.charAt(li) != '\'') {
-                                ++li;
-                            }
-                            String newImgUrl = tempLine.substring(5, li);
-                            tempLine = tempLine.substring(li);
-                            boolean isNeeded = true;
-                            String fileName = newImgUrl.substring(newImgUrl.lastIndexOf("/"));
-                            if (oldImgUrls.contains(newImgUrl)
-                                    || newImgUrl.contains("-lp")
-                                    || newImgUrl.contains("ajax")
-                                    || newImgUrl.charAt(0) == '/'
-                                    || fileName.charAt(2) == '_'
-                                    || fileName.charAt(2) == '-') {
-                                isNeeded = false;
-                            }
-                            if (isNeeded) {
-                                System.out.println("img url: " + newImgUrl);
-                                imgUrls.push(newImgUrl);
-                                oldImgUrls.add(newImgUrl);
-                            }
-                        }
-                    } while (li != 0 && (tempLine.contains(srcAtt) || tempLine.contains(imgTag)));
-                    bufin.close();
+                    } while(srcI > 0 || imgI > 0);
                 }
+                bufin.close();
             }
             is.close();
         } catch (IOException | NullPointerException e) {
             e.printStackTrace();
         }
+    }
+
+    private String parseSrc(String line, String srcUrl) {
+        int start = line.indexOf(srcAtt), end = start + 6;
+        if (start > 0) {
+            while (end < line.length()
+                    && line.charAt(end) != '\"'
+                    && line.charAt(end) != '\'') {
+                ++end;
+            }
+            String newSrcUrl = line.substring(start+5, end);
+            line = line.substring(end);
+            if (newSrcUrl.charAt(0) == '/') {
+                newSrcUrl = MAIN_DOMAIN + newSrcUrl;
+            } else if (newSrcUrl.length() < 13 && newSrcUrl.indexOf("htm") > 0) {
+                newSrcUrl = srcUrl.substring(0, srcUrl.lastIndexOf("/") + 1) + newSrcUrl;
+            }
+            boolean isNeeded = true;
+            for (String s : srcKeyWords) {
+                if (newSrcUrl.indexOf(s) > 0) {
+                    isNeeded = true;
+                    break;
+                }
+            }
+            for (String s : srcNoWords) {
+                if (newSrcUrl.indexOf(s) > 0) {
+                    isNeeded = false;
+                    break;
+                }
+            }
+            if (oldSrcUrls.contains(newSrcUrl)
+                    || newSrcUrl.length() == 0
+                    || !newSrcUrl.contains("http")
+                    || !newSrcUrl.contains("zhaofuli")) {
+                isNeeded = false;
+            }
+            if (isNeeded) {
+                System.out.println("src url: " + newSrcUrl);
+                srcUrls.push(newSrcUrl);
+                oldSrcUrls.add(newSrcUrl);
+            }
+        }
+        return line;
+    }
+
+    private String parseImg(String line) {
+        int start = line.indexOf(imgAtt), end = start + 5;
+        if (start > 0) {
+            while (end < line.length() && line.charAt(end) != '\"' && line.charAt(end) != '\'') {
+                ++end;
+            }
+            String newImgUrl = line.substring(start + 5, end);
+            line = line.substring(end);
+            boolean isNeeded = true;
+            String fileName = newImgUrl.substring(newImgUrl.lastIndexOf("/"));
+            if (oldImgUrls.contains(newImgUrl)
+                    || newImgUrl.contains("-lp")
+                    || newImgUrl.contains("ajax")
+                    || newImgUrl.charAt(0) == '/'
+                    || fileName.charAt(2) == '_'
+                    || fileName.charAt(2) == '-') {
+                isNeeded = false;
+            }
+            if (isNeeded) {
+                System.out.println("img url: " + newImgUrl);
+                imgUrls.push(newImgUrl);
+                oldImgUrls.add(newImgUrl);
+            }
+        }
+        return line;
     }
 
     @Override
@@ -133,8 +168,5 @@ public class MyHTMLParser implements Runnable{
 
     }
 
-    private Set<String> srcKeyWords = new HashSet<>();
-
-    private Set<String> srcNoWords = new HashSet<>();
 
 }
