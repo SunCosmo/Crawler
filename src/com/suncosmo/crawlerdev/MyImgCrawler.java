50709b5ef9859dd5f857895cab642cbef98feb73
lalamida <suncosmo@163.com>
2017-04-19 22:20:53 +0800

    improve ImgCrawler

--- src/com/suncosmo/crawlerdev/MyImgCrawler.java
+++ src/com/suncosmo/crawlerdev/MyImgCrawler.java
@@ -10,12 +10,12 @@ import java.util.Map;
 
 import static com.suncosmo.crawlerdev.Env.IMG_DOWNLOAD_PATH;
 import static com.suncosmo.crawlerdev.Env.imgUrls;
-import static com.suncosmo.crawlerdev.Env.oldImgUrls;
 
 /**
  * Created by SunCosmo on 2017/3/4.
  */
-public class MyImgCrawler implements Runnable {
+
+class MyImgCrawler implements Runnable {
     private static String method = "GET";
 
     private static Map<String, String> header;
@@ -27,38 +27,40 @@ public class MyImgCrawler implements Runnable {
 
         header.put("Referer", "http://zhaofuli.biz/luyilu/2016/1023/1112.html");
     }
+    
     public void downloadImg(String imgUrl) {
         HttpEvent.HttpConn http = new HttpEvent().new HttpConn(method, imgUrl, header, "");
-        do {
-            try {
-                byte inbuf[] = new byte[65535];
-                InputStream is = HttpEvent.getHttpIS(http);
+        try {
+            byte inf[] = new byte[65535];
+            InputStream is = HttpEvent.getHttpIS(http);
+            if(is != null) {
                 String format = imgUrl.substring(imgUrl.lastIndexOf("."));
-                String name = imgUrl.substring(imgUrl.lastIndexOf("/") + 1, imgUrl.lastIndexOf(".")) + "-" + (new Date()).getTime() +format;
+                String name = imgUrl.substring(imgUrl.lastIndexOf("/") + 1, imgUrl.lastIndexOf(".")) + "-" + (new Date()).getTime() + format;
                 File newFile = new File(IMG_DOWNLOAD_PATH + name);
-                if (!newFile.getParentFile().exists()) {
-                    newFile.getParentFile().mkdirs();
+                if (!newFile.getParentFile().exists() && !newFile.getParentFile().mkdir()) {
+                    throw new Exception("Failed to create local parent folder");
                 }
-                newFile.createNewFile();
-                FileOutputStream fo = new FileOutputStream(newFile);
-                int len;
-                while ((len = is.read(inbuf)) > 0) {
-                    fo.write(inbuf, 0, len);
+                if (newFile.createNewFile()) {
+                    FileOutputStream fo = new FileOutputStream(newFile);
+                    int len;
+                    while ((len = is.read(inf)) > 0) {
+                        fo.write(inf, 0, len);
+                    }
+                    System.out.println("download img: " + name);
+                    fo.close();
                 }
-                System.out.println("download img: " + name);
-                fo.close();
                 is.close();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        } while(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         for(;;) {
             if(imgUrls.isEmpty()) {
-                System.out.println("no img url left...sleeping ...");
+                System.out.println("no img url left...sleeping...");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
